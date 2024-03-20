929e02fd8f6cdcc497de57b1d9daa934e75246a5
Daria Matveeva <119957276+DannyOhDanny@users.noreply.github.com>
2024-03-20 04:26:59 +0300

    Feat: ProjectStage styles

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,15 @@
+import { ProjectStage } from 'src/utils/ProjectStage';
 import { Title } from './styled';
 
 const MainPage = () => {
   return (
-    <Title>
-      Установка систем безопасности в Новосибирске и Новосибирской области для
-      дома и Вашего бизнеса
-    </Title>
+    <>
+      <Title>
+        Установка систем безопасности в Новосибирске и Новосибирской области для
+        дома и Вашего бизнеса
+      </Title>
+      <ProjectStage />
+    </>
   );
 };
 
---
+++ src/utils/ProjectStage/ui/styled.ts
@@ -0,0 +1,9 @@
+import colors from 'src/shared/constants/colors';
+
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: ${colors.accentNegative};
+`;
+
+export { Title };
